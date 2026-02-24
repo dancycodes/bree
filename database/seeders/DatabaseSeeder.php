4cf9cb70640bbd25559d35ec010891f413fd8aab
gouater <139002105+dancycodes@users.noreply.github.com>
2026-02-24 19:01:24 +0100

    F-002: Database Configuration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::firstOrCreate(
+            ['email' => 'admin@breefondation.org'],
+            [
+                'name' => 'Super Admin',
+                'password' => bcrypt('Admin@Bree2025!'),
+            ]
+        );
     }
 }
